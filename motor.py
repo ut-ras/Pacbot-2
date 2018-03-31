9b9f57792c8bad1717fda7a4186bb3928d16b9e3
Joseph Ryan <josephryan3.14@gmail.com>
2018-03-31 18:57:42 -0500

    Clean up motor code and add encoder support
    note: none of this is tested

--- motor.py
+++ motor.py
@@ -1,146 +1,126 @@
 import RPIO
 from RPIO import PWM
-import time
+from math import copysign
 
 TOP_PIN_1 = 23
 TOP_PIN_2 = 24
 TOP_PIN_PWM = 3
+TOP_ENC_A = 0
+TOP_ENC_B = 0
 
 LEFT_PIN_1 = 7
 LEFT_PIN_2 = 8
 LEFT_PIN_PWM = 2
+LEFT_ENC_A = 0
+LEFT_ENC_B = 0
 
 BOTTOM_PIN_1 = 11
 BOTTOM_PIN_2 = 25
 BOTTOM_PIN_PWM = 18
+BOTTOM_ENC_A = 0
+BOTTOM_ENC_B = 0
 
 RIGHT_PIN_1 = 27
 RIGHT_PIN_2 = 14
 RIGHT_PIN_PWM = 17
+RIGHT_ENC_A = 0
+RIGHT_ENC_B = 0
+
+# non blocking interrupt enable
+RPIO.wait_for_interrupts(threaded=True)
 
 
 class Motor:
-    def __init__(self, pin1, pin2, dma_pwm, dma_channel=0):
+    def __init__(self, pin1, pin2, pwm_pin, encA, encB, dma_channel=0):
         self.dma_channel = dma_channel
-        self.dma_pwm = dma_pwm
+        self.pwm_pin = pwm_pin
         self.pin1 = pin1
         self.pin2 = pin2
-
+        self.encA = encA
+        self.encB = encB
+        self.speed = 0
+        self.ticks = 0
+        # Set up gpio pins, encoder interupts, and dma
         PWM.init_channel(dma_channel, subcycle_time_us=8000)
-        RPIO.setup(dma_pwm, RPIO.OUT)
+        RPIO.setup(pwm_pin, RPIO.OUT)
         RPIO.setup(pin1, RPIO.OUT)
         RPIO.setup(pin2, RPIO.OUT)
+        RPIO.setup(encB, RPIO.IN)
+        RPIO.add_interrupt_callback(
+            encA,
+            self.encCallback,
+            edge='rising',
+            threaded_callback=True)  # todo: see if this makes a difference
 
-    def set_motor(self, mode):
-        if mode == 0:
-            RPIO.output(self.pin1, False)
-            RPIO.output(self.pin2, False)
-        elif mode == -1:
-            RPIO.output(self.pin1, False)
-            RPIO.output(self.pin2, True)
-        elif mode == 1:
-            RPIO.output(self.pin2, False)
-            RPIO.output(self.pin1, True)
+    def encCallback(self):
+        if RPIO.input(self.encB):
+            self.ticks += 1
         else:
-            raise ValueError('Invalid motor mode')
+            self.ticks -= 1
 
-    def set_speed(self, percent_speed):
-        PWM.add_channel_pulse(self.dma_channel, self.dma_pwm, start=0, width=self.calc_width(percent_speed))
+    def set(self, speed):
+        """manipulates hardware to turn at given speed in range [-1,1] (positive
+        should be clockwise)"""
+        # todo: verify that positive is clockwise for each motor
+        mysign = copysign(1, self.speed)
+        newsign = copysign(1, speed)
+        self.speed = speed
+        if mysign != newsign:
+            if newsign == 0:
+                RPIO.output(self.pin1, False)
+                RPIO.output(self.pin2, False)
+            elif newsign == -1:
+                RPIO.output(self.pin1, False)
+                RPIO.output(self.pin2, True)
+            elif newsign == 1:
+                RPIO.output(self.pin2, False)
+                RPIO.output(self.pin1, True)
+        self.set_speed(abs(self.speed * 100))
 
-    def calc_width(self, percent_on):
-        if percent_on < 0 or percent_on > 100:
-            raise ValueError('Percent speed must be between 0-100')
-        print(PWM.get_channel_subcycle_time_us(self.dma_channel))
-        print(int(((percent_on / 100) * (PWM.get_channel_subcycle_time_us(self.dma_channel))) // 10))
-        return int(((percent_on / 100) * (PWM.get_channel_subcycle_time_us(self.dma_channel))) // 10)
+    def set_speed(self, speed):
+        """adds a pwm pulse for the given speed in range [-1,1]"""
+        PWM.add_channel_pulse(self.dma_channel, self.pwm_pin, start=0, width=(
+            speed * PWM.get_channel_subcycle_time_us(self.dma_channel)) // 10)
+
+    def __str__(self):
+        return "Speed: " + str(self.speed) + "\t\tTicks: " + str(self.ticks)
 
 
 class Base:
 
     def __init__(self):
-        self.disable()
+        #  self.disable()   # is this needed?
         PWM.setup()
-        self.motorTop = Motor(TOP_PIN_1, TOP_PIN_2, TOP_PIN_PWM, 0)
-        self.motorBottom = Motor(BOTTOM_PIN_1, BOTTOM_PIN_2, BOTTOM_PIN_PWM, 1)
-        self.motorLeft = Motor(LEFT_PIN_1, LEFT_PIN_2, LEFT_PIN_PWM, 2)
-        self.motorRight = Motor(RIGHT_PIN_1, RIGHT_PIN_2, RIGHT_PIN_PWM, 3)
+        self.top = Motor(TOP_PIN_1, TOP_PIN_2, TOP_PIN_PWM, TOP_ENC_A, TOP_ENC_B, 0)
+        self.bottom = Motor(BOTTOM_PIN_1, BOTTOM_PIN_2, BOTTOM_ENC_A, BOTTOM_ENC_B,
+                BOTTOM_PIN_PWM, 1)
+        self.left = Motor(LEFT_PIN_1, LEFT_PIN_2, LEFT_PIN_PWM, LEFT_ENC_A,
+                LEFT_ENC_B, 2)
+        self.right = Motor(RIGHT_PIN_1, RIGHT_PIN_2, RIGHT_PIN_PWM, RIGHT_ENC_A,
+                RIGHT_ENC_B, 3)
 
-    def move_dir_time(self, x_percent_speed, y_percent_speed, time_sec):
-        self.move_dir(x_percent_speed, y_percent_speed)
-        time.sleep(time_sec)
-        self.stop()
+    def move(self, x, y):
+        """sets motors to move linearly with x and y in range [-1,1]"""
+        self.top.set(x)
+        self.bottom.set(-x)
+        self.left.set(y)
+        self.right.set(-y)
 
-    def move_dir(self, x_percent_speed, y_percent_speed):
-        if x_percent_speed < 0:
-            self.motorTop.set_motor(1)
-            self.motorBottom.set_motor(-1)
-        elif x_percent_speed > 0:
-            self.motorTop.set_motor(-1)
-            self.motorBottom.set_motor(1)
-        else:
-            self.motorTop.set_motor(0)
-            self.motorBottom.set_motor(0)
-        self.motorTop.set_speed(abs(x_percent_speed))
-        self.motorBottom.set_speed(abs(x_percent_speed))
-
-        if y_percent_speed < 0:
-            self.motorLeft.set_motor(1)
-            self.motorRight.set_motor(-1)
-        elif y_percent_speed > 0:
-            self.motorLeft.set_motor(-1)
-            self.motorRight.set_motor(1)
-        else:
-            self.motorLeft.set_motor(0)
-            self.motorRight.set_motor(0)
-        self.motorLeft.set_speed(abs(y_percent_speed))
-        self.motorRight.set_speed(abs(y_percent_speed))
-
-    def move(self, direction, percent_speed):
-        if direction == 'left':
-            self.motorTop.set_speed(percent_speed)
-            self.motorBottom.set_speed(percent_speed)
-
-            self.motorRight.set_motor(0)
-            self.motorLeft.set_motor(0)
-            self.motorTop.set_motor(1)
-            self.motorBottom.set_motor(-1)
-
-        elif direction == 'right':
-            self.motorTop.set_speed(percent_speed)
-            self.motorBottom.set_speed(percent_speed)
-
-            self.motorLeft.set_motor(0)
-            self.motorRight.set_motor(0)
-            self.motorTop.set_motor(-1)
-            self.motorBottom.set_motor(1)
-
-        elif direction == 'up':
-            self.motorLeft.set_speed(percent_speed)
-            self.motorRight.set_speed(percent_speed)
-
-            self.motorTop.set_motor(0)
-            self.motorBottom.set_motor(0)
-            self.motorLeft.set_motor(1)
-            self.motorRight.set_motor(-1)
-
-        elif direction == 'down':
-            self.motorLeft.set_speed(percent_speed)
-            self.motorRight.set_speed(percent_speed)
-
-            self.motorTop.set_motor(0)
-            self.motorBottom.set_motor(0)
-            self.motorLeft.set_motor(-1)
-            self.motorRight.set_motor(1)
-
-        else:
-            raise ValueError('Invalid move direction')
+    def rotate(self, speed):
+        """turns at given speed in range [-1,1]"""
+        self.top.set(speed)
+        self.bottom.set(speed)
+        self.left.set(speed)
+        self.right.set(speed)
 
     def stop(self):
-        self.motorTop.set_motor(0)
-        self.motorLeft.set_motor(0)
-        self.motorBottom.set_motor(0)
-        self.motorRight.set_motor(0)
+        self.move(0, 0)
 
-    def disable(self):
+    def __str__(self):
+        return "Top: " + str(self.top) + "\nBottom: " + str(self.bottom) \
+            + "\nLeft: " + str(self.left) + "\nRight: " + str(self.right)
+
+    def __del__(self):
+        self.stop()
         PWM.cleanup()
         RPIO.cleanup()
--- test.py
+++ test.py
@@ -1,53 +1,16 @@
-from motor import Base
 import time
+import motor
 
-base = Base()
+testMotor = motor.Motor(23, 24, 3, 0, 0)
 
-# base.move('left', 50)
-# time.sleep(1)
-# base.stop()
-#
-# base.move('down', 50)
-# time.sleep(1)
-# base.stop()
-#
-# base.move('right', 50)
-# time.sleep(1)
-# base.stop()
-#
-# base.move('up', 50)
-# time.sleep(1)
-# base.stop()
+print(testMotor)
+testMotor.set(.6)
+time.sleep(1)
+testMotor.set(0)
+print(testMotor)
+time.sleep(1)
+testMotor.set(-.6)
+time.sleep(1)
+testMotor.set(0)
+print(testMotor)
 
-
-
-# base.motorTop.set_motor(1)
-# base.motorTop.set_speed(60)
-# time.sleep(1)
-# base.motorTop.set_motor(0)
-# base.motorLeft.set_motor(1)
-# base.motorLeft.set_speed(60)
-# time.sleep(1)
-# base.motorLeft.set_motor(0)
-# base.motorBottom.set_motor(1)
-# base.motorBottom.set_speed(60)
-# time.sleep(1)
-# base.motorBottom.set_motor(0)
-# base.motorRight.set_motor(1)
-# base.motorRight.set_speed(60)
-# time.sleep(1)
-# base.motorRight.set_motor(0)
-# base.disable()
-
-
-base.motorTop.set_motor(1)
-base.motorTop.set_speed(60)
-base.motorLeft.set_motor(1)
-base.motorLeft.set_speed(60)
-base.motorBottom.set_motor(-1)
-base.motorBottom.set_speed(60)
-base.motorRight.set_motor(-1)
-base.motorRight.set_speed(60)
-time.sleep(5)
-base.stop()
-base.disable()
