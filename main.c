a95363e92560909f46c6f8ce3337d569ddc541b1
Joseph Ryan <josephryan3.14@gmail.com>
2018-04-17 18:47:12 -0500

    Trapezoidal motor profile testing

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -7,12 +8,9 @@
 #include "enc.h"
 #include "pid.h"
 
-double test_speed = .4;
-
-PID* controller;
-Motor* m;
-double dt = .01; // fastest possible callbacks with pigpio
+double dt = .05; // fastest possible callbacks with pigpio
 double maxSpeed = .6;
+double test_speed = .4;
 
 Motor* motors[4];
 PID* pids[4];
@@ -20,31 +18,33 @@ PID* pids[4];
 
 void cleanup() {
     signal(SIGINT, SIG_IGN);
-    stop();
     terminateBase();
     gpioTerminate();
     printf("\nGPIO Terminated\n");
     exit(0);
 }
 
-void testIndividualMotor(Motor* m) {
-    setMotor(m, test_speed);
-    time_sleep(.5);
-    setMotor(m, -test_speed);
-    time_sleep(.5);
-    stopMotor(m);
-    time_sleep(.5);
+void testIndividualMotor() {
+    Motor* m = createMotor(15, 14, 18, 17, 27);
+    double duty = 0;
+    double freq = 500;
+    while(duty != -1) {
+        gpioSetPWMfrequency(18, freq);   // lower freq gives lower min speed
+        setMotor(m, duty);
+        scanf("%lf %lf", &duty, &freq);
+    }
+    destroyMotor(m);
 }
 
-void testBase() {
+int mainBase() {
     signal(SIGINT, cleanup);
-    Motor* top = createMotor(15, 14, 18, 27, 17);
+    Motor* top = createMotor(15, 14, 18, 17, 27);
+    Motor* left = createMotor(10, 9, 11, 8, 25);
+    Motor* bottom = createMotor(16, 20, 21, 26, 19);
+    Motor* right = createMotor(5, 6, 13, 1, 7);
     testIndividualMotor(top);
-    Motor* left = createMotor(10, 9, 11, 25, 8);
     testIndividualMotor(left);
-    Motor* bottom = createMotor(16, 20, 21, 19, 26);
     testIndividualMotor(bottom);
-    Motor* right = createMotor(5, 6, 13, 7, 1);
     testIndividualMotor(right);
     initBase(top, bottom, left, right);
     printEncoders();
@@ -57,36 +57,38 @@ void testBase() {
         test_speed = -test_speed;
     }
     cleanup();
+    return 0;
 }
 
 void baseCB() {
+    double next[4];
     for(int i = 0; i < 4; ++i) {
-        double next = updatePID(pids[i], motors[i]->enc->pos, dt);
-        if(next > maxSpeed) {
-            next = maxSpeed;
-        } else if(next < -maxSpeed) {
-            next = -maxSpeed;
+        next[i] = updatePID(pids[i], motors[i]->enc->pos, dt);
+        if(next[i] > maxSpeed) {
+            next[i] = maxSpeed;
+        } else if(next[i] < -maxSpeed) {
+            next[i] = -maxSpeed;
         }
-        // printf("Enc: %ld\t\tMotor: %f\n", m->enc->pos, next);
-        setMotor(motors[i], next);
     }
+    driveDirect(next[0], next[1], next[2], next[3]);
 }
 
 int main() {
     if (gpioInitialise() < 0) {
         return 1;
     }
-    Motor* top = createMotor(15, 14, 18, 17, 27);
-    Motor* left = createMotor(10, 9, 11, 8, 25);
-    Motor* bottom = createMotor(16, 20, 21, 26, 19);
-    Motor* right = createMotor(5, 6, 13, 1, 7);
-    motors[0] = top;
-    motors[1] = bottom;
-    motors[2] = left;
-    motors[3] = right;
+    // testIndividualMotor();
+    // return 0;
+    signal(SIGINT, cleanup);
+    motors[0] = createMotor(15, 14, 18, 17, 27);
+    motors[1] = createMotor(16, 20, 21, 26, 19);
+    motors[2] = createMotor(10, 9, 11, 8, 25);
+    motors[3] = createMotor(5, 6, 13, 1, 7);
+    initBase(motors[0], motors[1], motors[2], motors[3]);
+
     char* logs[] = {"top.txt", "bot.txt", "left.txt", "right.txt"};
     for(int i = 0; i < 4; ++i) {
-        pids[i] = createPID(.01, .0003, .0005, logs[i]);
+        pids[i] = createPID(.001, .0000, .0000, logs[i]);
         setPID(pids[i], 0);
     }
 
@@ -98,8 +100,75 @@ int main() {
         printf("Input next set point (-1 to exit): ");
         scanf("%d", &setPoint);
     }
-    for(int i=0;i<4;++i){
-        destroyMotor(motors[i]);
+    // while(1){
+    //     printf("%.5f %.5f\n", pids[2]->prev, pids[2]->sum);
+    // }
+    for(int i = 0; i < 4; ++i) {
         destroyPID(pids[i]);
     }
+    cleanup();
+    return 0;
 }
+
+double deltaMotor = .01;
+double maxMotor = .5;
+double minMotor = .1;
+int deadzone = 10;
+double currentSpeed = 0;
+int setPoint = 0;
+int startPoint = 0;
+int done = 1;
+double dist = 0;
+void trapezoidCallback() {
+    if(done) {
+        return;
+    }
+    int ticks = motors[0]->enc->pos;
+    // printf("%.3lf\t%d\n", currentSpeed, ticks);
+    if(fabs(dist) - abs(ticks) < deadzone) {
+        done = 1;
+        printf("we did it: %d\n", motors[0]->enc->pos);
+        currentSpeed = 0;
+        motors[0]->enc->pos = 0;
+        stopMotor(motors[0]);
+        stopMotor(motors[1]);
+        return;
+    }
+    if(abs(ticks) > dist / 2) {
+        // slow
+        currentSpeed -= deltaMotor;
+    } else {
+        // fast
+        currentSpeed += deltaMotor;
+    }
+    if(fabs(currentSpeed) > maxSpeed) {
+        // flat
+        setMotor(motors[0], maxSpeed);
+        setMotor(motors[1], -maxSpeed);
+    } else {
+        setMotor(motors[0], currentSpeed);
+        setMotor(motors[1], -currentSpeed);
+    }
+}
+
+int mainTrap() {
+    if (gpioInitialise() < 0) {
+        return 1;
+    }
+    signal(SIGINT, cleanup);
+    motors[0] = createMotor(15, 14, 18, 17, 27);
+    motors[1] = createMotor(16, 20, 21, 26, 19);
+    motors[2] = createMotor(10, 9, 11, 8, 25);
+    motors[3] = createMotor(5, 6, 13, 1, 7);
+    gpioSetTimerFunc(0, dt * 1000, trapezoidCallback);
+    while(setPoint != -1) {
+        printf("Input next set point (-1 to exit): ");
+        scanf("%d", &setPoint);
+        done = 0;
+        dist = abs(setPoint - startPoint);
+        // printf("%d\n", motors[0]->enc->pos);
+        // time_sleep(.5);
+    }
+    cleanup();
+}
+
